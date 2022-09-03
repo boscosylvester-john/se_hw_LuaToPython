16379ff6961f3e1358831bcabb60d88d301131f2
Boscosylvester John Chittilapilly <boscosylvesterjohn@gmail.com>
2022-09-03 09:08:01 -0400

    Add README.md base file, update setup.py and create project package

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open('README.md') as f:
     readme = f.read()
 
 setup(
-    name='SE HW 1',
+    name='SE HW LUA to Python',
     version='1.0',
-    description='SE HW 1',
+    description='SE HW 2 3 4 5',
     long_description=readme,
     author='SE Group 10',
-    url='https://github.com/boscosylvester-john/se_hw1',
+    url='https://github.com/boscosylvester-john/se_hw_LuaToPython',
     packages=find_packages(exclude=('tests', 'docs'))
 )
--- src/__init__.py
+++
@@ -1,1 +0,0 @@
-from .calculations import add, subtract, multiply, divide, get_input, get_operator
