796b5b7040e71ab603ee663c497b485711b69e39
ankurbanerji3 <ankurbanerji3@gmail.com>
2022-09-05 22:41:35 -0400

    Updated Sym Class

--- src/Sym.py
+++ src/Sym.py
@@ -13,4 +13,18 @@ class Sym:
         if v != "?":
             self.n = self.n + 1
             self._has[v] =  1 + (self._has[v])
-           
+    
+    def mid(self):
+      mode = most = -1
+      for k, v in self._has.items():
+        if v > most:
+          mode = k
+          most = v
+      return mode
+    
+    def div(self):
+      e = 0
+      for k, v in self._has.items():
+        if v > 0:
+          e = e - logarithm(v/self.n)
+      return e
--- src/Utils.py
+++ src/Utils.py
@@ -4,3 +4,6 @@ def per(t, p = 0.5):
     p = math.floor((p * len(t)) + 0.5)
     ind = max(1, min(len(t), p))
     return t[ind]
+
+def logarithm(p):
+    return p*math.log2(p)
