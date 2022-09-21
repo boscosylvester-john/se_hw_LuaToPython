cb0ea08b90cb542324138a75fc9b0d45aebea477
shlokio <112107528+shlokio@users.noreply.github.com>
2022-09-20 20:32:21 -0400

    Fix file calling methods

--- tests/test_data.py
+++ tests/test_data.py
@@ -5,13 +5,13 @@ import unittest
 
 class TestingData(unittest.TestCase):
   def test_data(self):
-    d = data.Data("../data/testdata.txt")
+    d = data.Data("data\\testdata.txt")
     for _,col in d.cols.y:
       utils.oo(col)
     return True
 
   def test_stats(self):
-    d = data.Data("../data/testdata.txt")
+    d = data.Data("data\\testdata.txt")
     print('xmid=', d.stats(2, d.cols.x, "mid"))
     print('xdiv=', d.stats(3, d.cols.x, "div"))
     print('ymid=', d.stats(2, d.cols.y, "mid"))
